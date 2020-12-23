7556c2c848d30a73da64df210fbfdb2629ba3f78
Reznychenko Sergey <sergeyrez.sr@gmail.com>
2020-12-23 09:48:39 +0200

    Fix 3

--- js/controls.js
+++ js/controls.js
@@ -9,11 +9,11 @@
   });
 });*/
 $(window).load(function () {
-    var phones = [{ mask: "+38 (0##) ### ## ##" }];
-    $("#phone").inputmask({
-      mask: phones});
+  var phones = [{ mask: "+38 (0##) ### ## ##" }];
+  $("#phone").inputmask({
+    mask: phones,
   });
-
+});
 
 function FormPhoneValidate() {
   let x = document.querySelector(".phone__rules-checkbox").checked;
@@ -37,7 +37,7 @@ function FormPhoneValidate() {
     "+38 (094)",
   ];
   //console.log(phoneValue.length);
-/*
+  /*
   if (phoneValue.length >= 12) {
     //document.querySelector(".phone__input-error").hidden = array.includes(phoneValue.substring(0, 9));
     if (!array.includes(phoneValue.substring(0, 9))) {
@@ -50,31 +50,42 @@ function FormPhoneValidate() {
     document.querySelector(".phone__input-value").style.borderColor = null;
     document.querySelector(".phone__input-error").hidden = true;
   }
+  */
+  /*
+console.log(phoneValue.substring(0,9));
+console.log(phoneValue.substring(0,9).indexOf('_'));
 */
+  //debugger;
 
-if(phoneValue.substring(0,9).indexOf('_') === -1){
-    document.querySelector(".phone__input-error").hidden = array.includes(phoneValue.substring(0, 9));
-    if (!array.includes(phoneValue.substring(0, 9))) {
-      document.querySelector(".phone__input-value").style.borderColor = " #e40428";
-    } 
-    else {
-      document.querySelector(".phone__input-value").style.borderColor = null;
-    }
-}
-else
-{
+  console.log(phoneValue.length);
+
+  if (phoneValue.length === 0) {
     document.querySelector(".phone__input-value").style.borderColor = null;
     document.querySelector(".phone__input-error").hidden = true;
-}
-
+  } else {
+    if (phoneValue.substring(0, 9).indexOf("_") === -1) {
+      document.querySelector(".phone__input-error").hidden = array.includes(
+        phoneValue.substring(0, 9)
+      );
+      if (!array.includes(phoneValue.substring(0, 9))) {
+        document.querySelector(".phone__input-value").style.borderColor = "#e40428";
+      } else {
+        document.querySelector(".phone__input-value").style.borderColor = null;
+      }
+    } else {
+      document.querySelector(".phone__input-value").style.borderColor = null;
+      document.querySelector(".phone__input-error").hidden = true;
+    }
+  }
+  /*
 console.log(phoneValue.indexOf('_') === -1);
 console.log(phoneValue.length)
 console.log(x)
 console.log(array.includes(phoneValue.substring(0, 9)));
-
+*/
   document.querySelector(".button-phone").disabled = !(
-    //phoneValue.length === 19 &&
-    (phoneValue.indexOf('_') === -1) &&
+    phoneValue.length === 19 &&
+    phoneValue.indexOf("_") === -1 &&
     x &&
     array.includes(phoneValue.substring(0, 9))
   );
