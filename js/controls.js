263036c370f9a8a065a537203d5accb03d6da51b
Reznychenko Sergey <sergeyrez.sr@gmail.com>
2020-12-23 09:26:45 +0200

    Fix 2

--- js/controls.js
+++ js/controls.js
@@ -36,8 +36,10 @@ function FormPhoneValidate() {
     "+38 (092)",
     "+38 (094)",
   ];
+  //console.log(phoneValue.length);
+/*
   if (phoneValue.length >= 12) {
-    document.querySelector(".phone__input-error").hidden = array.includes(phoneValue.substring(0, 9));
+    //document.querySelector(".phone__input-error").hidden = array.includes(phoneValue.substring(0, 9));
     if (!array.includes(phoneValue.substring(0, 9))) {
       document.querySelector(".phone__input-value").style.borderColor = " #e40428";
     } 
@@ -48,9 +50,31 @@ function FormPhoneValidate() {
     document.querySelector(".phone__input-value").style.borderColor = null;
     document.querySelector(".phone__input-error").hidden = true;
   }
+*/
+
+if(phoneValue.substring(0,9).indexOf('_') === -1){
+    document.querySelector(".phone__input-error").hidden = array.includes(phoneValue.substring(0, 9));
+    if (!array.includes(phoneValue.substring(0, 9))) {
+      document.querySelector(".phone__input-value").style.borderColor = " #e40428";
+    } 
+    else {
+      document.querySelector(".phone__input-value").style.borderColor = null;
+    }
+}
+else
+{
+    document.querySelector(".phone__input-value").style.borderColor = null;
+    document.querySelector(".phone__input-error").hidden = true;
+}
+
+console.log(phoneValue.indexOf('_') === -1);
+console.log(phoneValue.length)
+console.log(x)
+console.log(array.includes(phoneValue.substring(0, 9)));
 
   document.querySelector(".button-phone").disabled = !(
-    phoneValue.length === 19 &&
+    //phoneValue.length === 19 &&
+    (phoneValue.indexOf('_') === -1) &&
     x &&
     array.includes(phoneValue.substring(0, 9))
   );
